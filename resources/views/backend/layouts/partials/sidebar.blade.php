0183f6216f05cb3c0ebced25cd0dfec3b136d227
datdev <dnguyentien145@gmail.com>
2024-12-12 13:02:02 +0700

    update codecode

--- resources/views/backend/layouts/partials/sidebar.blade.php
+++ resources/views/backend/layouts/partials/sidebar.blade.php
@@ -2,9 +2,9 @@
     <div class="sidebar-logo">
         <!-- Logo Header -->
         <div class="logo-header" data-background-color="dark">
-            <a href="{{ url('/') }}" class="logo">
-                <img src="{{ asset('backend/assets/img/kaiadmin/logo_light.svg') }}" alt="navbar brand"
-                    class="navbar-brand" height="20" />
+            <a href="https://sgomedia.vn/" class="logo">
+                <img src="{{ asset('backend/assets/img/logo-sgo-media-optimized.png') }}" alt="navbar brand"
+                    class="navbar-brand img-fluid" height="20" />
             </a>
             <div class="nav-toggle">
                 <button class="btn btn-toggle toggle-sidebar">
@@ -23,81 +23,63 @@
     <div class="sidebar-wrapper scrollbar scrollbar-inner">
         <div class="sidebar-content">
             <ul class="nav nav-secondary">
-                <li class="nav-item active">
-                    <a href="{{ url('/') }}">
+                <li class="nav-item {{ Request::routeIs('admin.dashboard') ? 'active' : '' }}">
+                    <a href="{{ route('admin.dashboard') }}">
                         <i class="fas fa-home"></i>
                         <p>Tổng quan</p>
                     </a>
                 </li>
 
-                <li class="nav-item">
+                <li class="nav-item {{ Request::routeIs('admin.settings') ? 'active' : '' }}">
                     <a href="{{ route('admin.settings') }}">
                         <i class="fas fa-cogs"></i>
                         <p>Cấu hình chung</p>
                     </a>
                 </li>
 
-                <li class="nav-item">
+                <li class="nav-item {{ Request::routeIs('admin.banners') ? 'active' : '' }}">
                     <a href="{{ route('admin.banners') }}">
                         <i class="fas fa-image"></i>
                         <p>Cấu Hình Banner</p>
                     </a>
                 </li>
 
-                <li class="nav-item">
+                <li class="nav-item {{ Request::routeIs('admin.videos') ? 'active' : '' }}">
                     <a href="{{ route('admin.videos') }}">
                         <i class="fas fa-video"></i>
                         <p>Cấu Hình Video</p>
                     </a>
                 </li>
 
-                <li class="nav-item">
+                <li class="nav-item {{ Request::routeIs('admin.pricings') ? 'active' : '' }}">
                     <a href="{{ route('admin.pricings') }}">
                         <i class="fas fa-file-invoice-dollar"></i>
                         <p>Cấu Hình Bảng Giá</p>
                     </a>
                 </li>
 
-                <li class="nav-item">
+                <li class="nav-item {{ Request::routeIs('admin.services') ? 'active' : '' }}">
                     <a href="{{ route('admin.services') }}">
                         <i class="fab fa-servicestack"></i>
                         <p>Cấu Hình Dịch Vụ</p>
                     </a>
                 </li>
 
-                <li class="nav-item">
+                <li class="nav-item {{ Request::routeIs('admin.facilities') ? 'active' : '' }}">
                     <a href="{{ route('admin.facilities') }}">
                         <i class="fas fa-window-restore"></i>
                         <p>Cấu Hình Tiện Ích</p>
                     </a>
                 </li>
 
-                <li class="nav-item">
+                <li class="nav-item {{ Request::routeIs('admin.introductions') ? 'active' : '' }}">
                     <a href="{{ route('admin.introductions') }}">
                         <i class="fas fa-pen-square"></i>
                         <p>Cấu Hình Giới Thiệu</p>
                     </a>
                 </li>
-
-                {{-- <li class="nav-item">
-                    <a data-bs-toggle="collapse" href="#base">
-                        <i class="fas fa-layer-group"></i>
-                        <p>Base</p>
-                        <span class="caret"></span>
-                    </a>
-                    <div class="collapse" id="base">
-                        <ul class="nav nav-collapse">
-                            <li>
-                                <a href="components/avatars.html">
-                                    <span class="sub-item">Avatars</span>
-                                </a>
-                            </li>
-
-                        </ul>
-                    </div>
-                </li> --}}
-
             </ul>
+
         </div>
     </div>
 </div>
