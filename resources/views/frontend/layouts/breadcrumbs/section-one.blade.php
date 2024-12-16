5dc15ab0960c9bcedb9decf75c6acd271e84dfea
datdev <dnguyentien145@gmail.com>
2024-12-16 17:28:04 +0700

    update codecode

--- resources/views/frontend/layouts/breadcrumbs/section-one.blade.php
+++ resources/views/frontend/layouts/breadcrumbs/section-one.blade.php
@@ -9,7 +9,7 @@
         title="YouTube video player" frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> --}}
-    <video width="100%" height="auto" controls>
+    <video width="100%" height="auto" controls autoplay muted>
         <source src="{{ showImage($video->file_path) }}" type="video/mp4">
         Your browser does not support the video tag.
     </video>
